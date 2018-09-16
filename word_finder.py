50e9d4dfded646dfed2187255a5e297d9eaabe28
oas <omar.agudo@gmail.com>
2018-09-16 22:57:43 +0200

    Add examples to the result

--- word_finder.py
+++ word_finder.py
@@ -1,30 +1,46 @@
 import unittest
 from typing import List
+import abc
 
 
 class FinderResult:
-    def __init__(self, meanings: List[str]):
+    def __init__(self, meanings: List[str], examples: List[str]):
         self.meanings = meanings
+        self.examples = examples
 
 
-class WordFinder:
+class WordFinder(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def find(self, word: str) -> FinderResult:
+        pass
+
+
+class DummyWordFinder(WordFinder):
     def __init__(self, from_lan: str, to_lan: str):
         self.to_lan = to_lan
         self.from_lan = from_lan
 
     def find(self, word: str) -> FinderResult:
-        return FinderResult(['palabra'])
+        return FinderResult(['palabra'],
+                            ['Confio en tu palabra', 'No tengo palabras'])
 
 
 class WordFinderTest(unittest.TestCase):
 
     def setUp(self):
-        self.finder = WordFinder('en', 'es')
+        self.finder = DummyWordFinder('en', 'es')
 
     def test_should_find_word_meanings(self):
         result = self.finder.find('word')
         assert len(result.meanings) == 1
         assert result.meanings[0] == 'palabra'
+
+    def test_should_find_word_examples(self):
+        result = self.finder.find('word')
+        assert len(result.examples) == 2
+        assert result.examples[0] == 'Confio en tu palabra'
+        assert result.examples[1] == 'No tengo palabras'
 
 
 if __name__ == '__main__':
